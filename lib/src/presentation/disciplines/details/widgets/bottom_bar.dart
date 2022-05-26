dccb970650641df336970076c3654125e0bd1159
mbaumgartenbr <mbaumgartenbr@gmail.com>
2022-05-26 20:05:34 -0300

    chore: extract BottomBar from discipline details

--- lib/src/presentation/disciplines/details/widgets/bottom_bar.dart
+++ lib/src/presentation/disciplines/details/widgets/bottom_bar.dart
@@ -13,92 +13,58 @@ class DisciplineDetailsBottomBar extends StatelessWidget {
   Widget build(BuildContext context) {
     late final DisciplineDetailsBloc bloc = context.read();
 
-    return SizedBox(
-      height: 80,
-      child: BottomAppBar(
-        child: Padding(
-          padding: AppPadding.horizontalMedium,
-          child: Row(
-            children: [
-              BottomBarButton(
-                icon: const Icon(Icons.group_add_rounded),
-                tooltip: 'Importar Alunos',
-                onPressed: () async {
-                  await showModalBottomSheet<void>(
-                    context: context,
-                    isScrollControlled: true,
-                    shape: kBottomSheetShapeBorder,
-                    builder: (_) => StudentsImportPage(
-                      discipline: bloc.discipline,
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(width: 8),
-              BottomBarButton(
-                icon: const Icon(Icons.manage_accounts),
-                tooltip: 'Editar Alunos',
-                onPressed: () {
-                  AppRouter.showStudentsForm(
-                    context: context,
-                    discipline: bloc.discipline,
-                  );
-                },
-              ),
-              const SizedBox(width: 8),
-              BottomBarButton(
-                icon: const RotatedBox(
-                  quarterTurns: -1,
-                  child: Icon(Icons.login),
-                ),
-                tooltip: 'Exportar Chamadas',
-                onPressed: () async {
-                  await showModalBottomSheet<void>(
-                    context: context,
-                    isScrollControlled: true,
-                    shape: kBottomSheetShapeBorder,
-                    builder: (_) => DisciplineExportPage(
-                      discipline: bloc.discipline,
-                    ),
-                  );
-                },
-              ),
-              const Spacer(),
-              FloatingActionButton(
-                child: const Icon(Icons.add_task_rounded),
-                onPressed: () {
-                  AppRouter.showAttendanceForm(context, bloc.discipline);
-                },
-              ),
-            ],
-          ),
-        ),
+    return BottomBar(
+      fab: FloatingActionButton(
+        child: const Icon(Icons.add_task_rounded),
+        onPressed: () {
+          AppRouter.showAttendanceForm(context, bloc.discipline);
+        },
       ),
-    );
-  }
-}
-
-class BottomBarButton extends StatelessWidget {
-  const BottomBarButton({
-    super.key,
-    required this.icon,
-    required this.tooltip,
-    this.onPressed,
-  });
-
-  final Widget icon;
-  final String tooltip;
-  final VoidCallback? onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: icon,
-      tooltip: tooltip,
-      onPressed: onPressed,
-      iconSize: 24,
-      splashRadius: 24,
-      color: Theme.of(context).colorScheme.onSurface,
+      buttons: [
+        BottomBarButton(
+          icon: const Icon(Icons.group_add_rounded),
+          tooltip: 'Importar Alunos',
+          onPressed: () async {
+            await showModalBottomSheet<void>(
+              context: context,
+              isScrollControlled: true,
+              shape: kBottomSheetShapeBorder,
+              builder: (_) => StudentsImportPage(
+                discipline: bloc.discipline,
+              ),
+            );
+          },
+        ),
+        const SizedBox(width: 8),
+        BottomBarButton(
+          icon: const Icon(Icons.manage_accounts),
+          tooltip: 'Editar Alunos',
+          onPressed: () {
+            AppRouter.showStudentsForm(
+              context: context,
+              discipline: bloc.discipline,
+            );
+          },
+        ),
+        const SizedBox(width: 8),
+        BottomBarButton(
+          icon: const RotatedBox(
+            quarterTurns: -1,
+            child: Icon(Icons.login),
+          ),
+          tooltip: 'Exportar Chamadas',
+          onPressed: () async {
+            await showModalBottomSheet<void>(
+              context: context,
+              isScrollControlled: true,
+              shape: kBottomSheetShapeBorder,
+              builder: (_) => DisciplineExportPage(
+                discipline: bloc.discipline,
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
--- lib/src/shared/widgets/widgets.dart
+++ lib/src/shared/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'bordered_column.dart';
+export 'bottom_bar.dart';
 export 'fade_upwards_switcher.dart';
 export 'snackbar.dart';
