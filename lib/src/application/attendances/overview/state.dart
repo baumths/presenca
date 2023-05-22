baa9be45b103bf7945d85418b0969a1e45c44c0e
baumths <baumths@gmail.com>
2023-05-22 14:33:51 -0300

    remove freezed & sanitize attendances/overview

--- lib/src/application/attendances/overview/state.dart
+++ lib/src/application/attendances/overview/state.dart
@@ -1,10 +1,14 @@
 part of 'bloc.dart';
 
-@freezed
-class AttendancesOverviewState with _$AttendancesOverviewState {
-  const factory AttendancesOverviewState.loading() = AttendancesOverviewLoading;
+sealed class AttendancesOverviewState {
+  const AttendancesOverviewState();
+}
 
-  const factory AttendancesOverviewState.success({
-    required List<Attendance> attendances,
-  }) = AttendancesOverviewSuccess;
+class AttendancesOverviewLoadInProgress extends AttendancesOverviewState {
+  const AttendancesOverviewLoadInProgress();
+}
+
+class AttendancesOverviewLoadSuccess extends AttendancesOverviewState {
+  const AttendancesOverviewLoadSuccess(this.attendances);
+  final List<Attendance> attendances;
 }
