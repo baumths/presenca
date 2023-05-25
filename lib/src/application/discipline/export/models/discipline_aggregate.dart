b7cf4dc2035af286e7321dbfa795c04f5fcd808c
baumths <baumths@gmail.com>
2023-05-25 13:25:01 -0300

    add `DisciplineAggregate.parseCsv()`

--- lib/src/application/discipline/export/models/discipline_aggregate.dart
+++ lib/src/application/discipline/export/models/discipline_aggregate.dart
@@ -15,6 +15,7 @@ class DisciplineAggregate {
   static const String notesTag = 'Anotações';
   static const String attendedTag = 'Presente';
   static const String inactiveTag = '(Inativo)';
+
   const DisciplineAggregate({
     required this.discipline,
     required this.students,
@@ -80,4 +81,130 @@ class DisciplineAggregate {
       yield studentRow;
     }
   }
+
+  // TODO(future): split & improve this method
+  /// Expected format:
+  ///
+  /// ```txt
+  /// Students                         Attendances ────────────── Example ──────── N ─▶
+  ///    │    | `nameTag`            | "dd/MM/yyyy HH:mm"  | "01/01/1970 00:00" | ... |
+  ///    │    | "John"               | `attendedTag` or "" | `attendedTag`      | ... |
+  ///    │    | "Mary `inactiveTag`" | `attendedTag` or "" | ""                 | ... |
+  ///    N    | ...                  | ...                 | ...                | ... |
+  ///    ▼    | `notesTag`           | ""                  | "Some note"        | ... |
+  /// ```
+  ///
+  /// The span at `table[0][0]` must be [DisciplineAggregate.nameTag].
+  /// The span at `table[table.length][0]` must be [DisciplineAggregate.notesTag].
+  ///
+  /// The simples accepted table is: `[[nameTag][notesTag]]`.
+  ///
+  /// - Returns an [InvalidTableFormat] failure if [table] does not match the
+  /// `[[nameTag, ...], ..., [notesTag, ...]]` pattern .
+  /// - Returns an [InvalidAttendanceDateFormat] failure if [attendanceDateFormat.parse]
+  /// fails to parse any attendance date & time by throwing a [FormatException].
+  static (DisciplineCsvParsingFailure?, DisciplineAggregate?) parseCsv(
+    Discipline discipline,
+    List<List<String>> table,
+  ) {
+    final isExpectedFormat = switch (table) {
+      [[nameTag, ...], ..., [notesTag, ...]] => true,
+      _ => false,
+    };
+
+    if (!isExpectedFormat) {
+      return (const InvalidTableFormat(), null);
+    }
+
+    final header = table[0];
+    final footer = table[table.length - 1];
+    final studentCount = table.length - 2;
+
+    final students = <Student>[];
+    final attendances = <Attendance>[];
+
+    for (int row = 1; row <= studentCount; ++row) {
+      String name = table[row][0];
+      bool active = true;
+
+      if (name.contains(inactiveTag)) {
+        active = false;
+        name = name.replaceAll(inactiveTag, '').trim();
+      }
+
+      final student = Student.empty().copyWith(
+        disciplineId: discipline.id,
+        name: name,
+        active: active,
+      );
+
+      students.add(student);
+    }
+
+    assert(students.length == studentCount);
+
+    for (int col = 1; col < header.length; ++col) {
+      final attendedStudentIds = <String>[];
+
+      for (int row = 1; row <= studentCount; ++row) {
+        if (table[row][col].isNotEmpty) {
+          final student = students[row - 1];
+          assert(table[row][0].contains(student.name));
+          attendedStudentIds.add(student.id);
+        }
+      }
+
+      try {
+        final attendance = Attendance.empty().copyWith(
+          disciplineId: discipline.id,
+          date: attendanceDateFormat.parse(header[col]),
+          note: footer[col],
+          attendedStudentIds: attendedStudentIds,
+        );
+
+        attendances.add(attendance);
+      } on FormatException {
+        assert(attendanceDateFormat.pattern != null);
+        return (
+          InvalidAttendanceDateFormat(
+            expected: attendanceDateFormat.pattern!,
+            actual: header[col],
+            column: col,
+          ),
+          null
+        );
+      }
+    }
+
+    return (
+      null,
+      DisciplineAggregate(
+        discipline: discipline,
+        students: students,
+        attendances: attendances,
+      )
+    );
+  }
+}
+
+sealed class DisciplineCsvParsingFailure {
+  const DisciplineCsvParsingFailure();
+}
+
+final class InvalidTableFormat extends DisciplineCsvParsingFailure {
+  const InvalidTableFormat();
+}
+
+final class InvalidAttendanceDateFormat extends DisciplineCsvParsingFailure {
+  const InvalidAttendanceDateFormat({
+    required this.expected,
+    required this.actual,
+    required this.column,
+  });
+
+  final String expected;
+  final String actual;
+
+  /// Zero indexed
+  final int column;
 }
