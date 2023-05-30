3245fa0744ea360d3ef1199d02841859d721f198
baumths <baumths@gmail.com>
2023-05-30 17:31:33 -0300

    add discipline import business logic

--- lib/src/application/discipline/form/bloc.dart
+++ lib/src/application/discipline/form/bloc.dart
@@ -3,6 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../../domain/discipline.dart';
+import '../../../domain/attendance.dart';
+import '../../../domain/student.dart';
+import '../../../infrastructure/adapters/file_picker_adapter.dart';
+import '../common/discipline_aggregate.dart';
 
 part 'bloc.freezed.dart';
 part 'event.dart';
@@ -12,12 +16,21 @@ class DisciplineFormBloc
     extends Bloc<DisciplineFormEvent, DisciplineFormState> {
   DisciplineFormBloc({
     required DisciplinesRepository disciplinesRepository,
+    required AttendancesRepository attendancesRepository,
+    required StudentsRepository studentsRepository,
+    required FilePickerAdapter filePicker,
   })  : _disciplinesRepository = disciplinesRepository,
+        _attendancesRepository = attendancesRepository,
+        _studentsRepository = studentsRepository,
+        _filePicker = filePicker,
         super(DisciplineFormState.initial()) {
     on<DisciplineFormEvent>(_onEvent);
   }
 
   final DisciplinesRepository _disciplinesRepository;
+  final AttendancesRepository _attendancesRepository;
+  final StudentsRepository _studentsRepository;
+  final FilePickerAdapter _filePicker;
 
   Future<void> _onEvent(
     DisciplineFormEvent event,
@@ -27,6 +40,8 @@ class DisciplineFormBloc
       DisciplineFormStarted event => _onStarted(event, emit),
       DisciplineFormNameChanged event => _onNameChanged(event, emit),
       DisciplineFormArchivePressed event => _onArchivePressed(event, emit),
+      DisciplineFormImportPressed event => _onImportPressed(event, emit),
+      DisciplineFormImportReset() => _onImportReset(emit),
       DisciplineFormSubmitted event => _onSubmitted(event, emit),
     };
   }
@@ -41,38 +56,94 @@ class DisciplineFormBloc
       return;
     }
 
-    emit(
-      state.copyWith(
-        discipline: editingDiscipline,
-        isEditing: true,
-        saveFailureOrSuccessOption: const None(),
-      ),
-    );
+    emit(state.copyWith(
+      discipline: editingDiscipline,
+      isEditing: true,
+      saveFailureOrSuccessOption: const None(),
+    ));
   }
 
   Future<void> _onNameChanged(
     DisciplineFormNameChanged event,
     Emitter<DisciplineFormState> emit,
-  ) async {
-    final String name = event.name.trim();
-
-    emit(
-      state.copyWith(
-        discipline: state.discipline.copyWith(name: name),
-        saveFailureOrSuccessOption: const None(),
-      ),
-    );
+  ) {
+    emit(state.copyWith(
+      discipline: state.discipline.copyWith(name: event.name.trim()),
+      saveFailureOrSuccessOption: const None(),
+    ));
+    return Future.value();
   }
 
   Future<void> _onArchivePressed(
     DisciplineFormArchivePressed event,
     Emitter<DisciplineFormState> emit,
-  ) async {
+  ) {
     emit(state.copyWith(
       discipline: state.discipline.copyWith(
         isArchived: !state.discipline.isArchived,
       ),
     ));
+    return Future.value();
+  }
+
+  Future<void> _onImportPressed(
+    DisciplineFormImportPressed event,
+    Emitter<DisciplineFormState> emit,
+  ) async {
+    if (state.isEditing) {
+      // Importing is disabled when editing a discipline
+      return;
+    }
+
+    emit(state.copyWith(
+      importResult: const DisciplineImportLoading(),
+    ));
+
+    final table = await _filePicker.pickCsv();
+
+    if (table.isEmpty) {
+      emit(state.copyWith(
+        importResult: const DisciplineImportInitial(),
+      ));
+      return;
+    }
+
+    final result = DisciplineAggregate.parseCsv(state.discipline, table);
+
+    switch (result) {
+      case (final failure?, null):
+        final message = switch (failure) {
+          InvalidTableFormat() => 'Formato de tabela inválido.',
+          InvalidAttendanceDateFormat failure =>
+            'Falha ao interpretar data e hora de chamada. '
+                'Linha 1, Coluna ${failure.column + 1}:\n'
+                'Formato recebido: "${failure.actual}"\n'
+                'Formato esperado: "dd/mm/aaaa hh:mm"',
+        };
+
+        emit(state.copyWith(
+          importResult: DisciplineImportFailure(message),
+        ));
+
+      case (null, final disciplineAggregate?):
+        emit(state.copyWith(
+          importResult: DisciplineImportSuccess(
+            students: disciplineAggregate.students,
+            attendances: disciplineAggregate.attendances,
+          ),
+        ));
+
+      case (null, null):
+        assert(false, 'Unreachable');
+        return;
+    }
+  }
+
+  Future<void> _onImportReset(Emitter<DisciplineFormState> emit) {
+    emit(state.copyWith(
+      importResult: const DisciplineImportInitial(),
+    ));
+    return Future.value();
   }
 
   Future<void> _onSubmitted(
@@ -85,13 +156,23 @@ class DisciplineFormBloc
 
     if (discipline.name.isNotEmpty) {
       failureOrSuccess = await _disciplinesRepository.save(discipline);
+
+      // TODO: handle [StudentFailure] and [AttendanceFailure]
+      await failureOrSuccess.fold(
+        (_) => Future.value(null),
+        (_) => switch (state.importResult) {
+          DisciplineImportSuccess(:final students, :final attendances) => (
+              _studentsRepository.save(discipline.id, students),
+              _attendancesRepository.save(discipline.id, attendances)
+            ).wait,
+          _ => Future.value(null),
+        },
+      );
     }
 
-    emit(
-      state.copyWith(
-        discipline: discipline,
-        saveFailureOrSuccessOption: optionOf(failureOrSuccess),
-      ),
-    );
+    emit(state.copyWith(
+      discipline: discipline,
+      saveFailureOrSuccessOption: optionOf(failureOrSuccess),
+    ));
   }
 }
