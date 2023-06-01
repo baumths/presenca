aa353839bd176cad0dac16d27ac39f5c19ab2936
baumths <baumths@gmail.com>
2023-05-31 22:13:34 -0300

    fix csv to list conversion

--- lib/src/infrastructure/adapters/file_picker_adapter.dart
+++ lib/src/infrastructure/adapters/file_picker_adapter.dart
@@ -1,4 +1,4 @@
-import 'dart:convert' show utf8;
+import 'dart:io' show File;
 
 import 'package:csv/csv.dart' show CsvToListConverter;
 import 'package:file_picker/file_picker.dart';
@@ -7,31 +7,27 @@ abstract class FilePickerAdapter {
   Future<List<List<String>>> pickCsv();
 }
 
+const _csvToListConverter = CsvToListConverter(
+  shouldParseNumbers: false,
+);
+
 class FilePickerAdapterImpl extends FilePickerAdapter {
   @override
   Future<List<List<String>>> pickCsv() async {
     final result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['text/csv', 'csv'],
-      withReadStream: true,
     );
 
     if (result == null || result.files.isEmpty) {
       return const [];
     }
 
-    final Stream<List<int>>? dataStream = result.files.single.readStream;
-
-    if (dataStream == null) {
+    try {
+      final content = await File(result.files.single.path!).readAsString();
+      return _csvToListConverter.convert<String>(content);
+    } on Exception {
       return const [];
     }
-
-    final List<List<String>> csv = await dataStream
-        .transform(utf8.decoder)
-        .transform(const CsvToListConverter())
-        .map((row) => row.map((dynamic obj) => '$obj').toList())
-        .toList();
-
-    return csv;
   }
 }
